0487fa02c084e3f77191dfccd3a9b570b85598d6
Shawn <shawnschulz@berkeley.edu>
2024-06-18 23:36:45 -0700

    Start testing and debugging shawn_matrix.cpp

--- shawn_matrix.cpp
+++ shawn_matrix.cpp
@@ -6,8 +6,11 @@ using namespace std;
 class smatrix {
 
 public:
-    void shape() {
+    void view() {
         cout << "row x cols: " << shape[0] << " " << shape[1] << endl;
+    }
+    array<int,2> shape(){
+        return shape;
     }
     smatrix(initializer_list<vector<float>> list){
        int args_index;
@@ -36,12 +39,23 @@ public:
         }
         cout << endl;
     }
-
+    vector<vector<float> getcols() {
+        return colsview
+    }
+    vector<vector<float> getrows() {
+        return rowsview
+    }
+    void setcols(vector<vector<float>> input) {
+        colsview = input
+    }
+    void setrows(vector<vector<float>> input) {
+        rowsview = input
+    }
 private:
     array<int,2> shape;
     vector<vector<float>> colsview;
     vector<vector<float>> rowsview;
-}
+};
 
 
 //#include <stdio.h>
@@ -58,64 +72,42 @@ float CPUDotProduct(vector<float> vec1, vector<float> vec2)
     }
     return sum;
 }
-
-float GPUDotProduct(vector<float> vec1, vector<float> vec2)
+smatrix NaiveMatMul(smatrix mat1, smatrix mat2)
 {
-}
-
-//if we're doing this should prob check that row indices are correct
-vector<vector<float>> NaiveMatMul(vector<vector<float>> mat1, vector<vector<float>> mat2)
-{
-    //columns of mat1 should be same size of rows of mat2
-    assert(mat1.size() == mat2[0].size());
-    int mat_1_col_index;
+    //rows of mat1 should be same size of columns of mat2
+    assert(mat1.shape()[0] == mat2.shape())[1];
+    int mat_1_col_index; 
     int mat_2_col_index;
-    int row_index;
+    vector<float> row_multiple;
     vector<float> column_multiple;
     float new_cell_value;
-    vector<vector<float>> output_mat;
-    vector<float> new_column;
+    initializer_list<vector<float>> output_list
+    vector<vector<float>> all_rows = mat1.getrows()
+    vector<vector<float>> all_columns = mat2.getcols()
 
-    for (row_index = 0; row_index < mat1[0].size(); row_index++)
+    for ( mat_2_col_index= 0;  mat_2_col_index < mat2.size(); mat_2_col_index++)
     {
-        vector<float> row_multiple;
-        for (mat_1_col_index = 0; mat_1_col_index < mat1.size(); mat_1_col_index++) 
+        for (mat_1_col_index = 0; mat_1_col_index < mat1[0].size(); mat_1_col_index++)
         {
-           row_multiple.push_back(mat1[mat_1_col_index][row_index]);
-        }
-        int mat_2_col_index;
-        for ( mat_2_col_index= 0;  mat_2_col_index < mat2.size(); mat_2_col_index++)
-        {
-            column_multiple = mat2[mat_2_col_index];
+            column_multiple = all_columns[mat_2_col_index];
+            row_multiple = all_rows[mat_1_col_index]
             new_cell_value = CPUDotProduct(column_multiple, row_multiple);
             new_column.push_back(new_cell_value);
         }
-        output_mat.push_back(new_column);
+        output_list.push_back(new_column);
     }
-    return output_mat;
+    return smatrix(output_list);
 }
-
+void GPUDotProduct(vector<float> vec1, vector<float> vec2)
+{
+}
+void GPUMatMul(smatrix mat1, smatrix mat2) 
+{
+}
 int main() {
-    vector<vector<float>> mat1;
-    vector<float> vect{10,20,30};
-    vector<float> vect2{20,30,40};
-    vector<float> vect3{21,31,41};
-    mat1.push_back(vect);
-    mat1.push_back(vect2);
-    mat1.push_back(vect3);
-    vector<vector<float>> mat2;
-    vector<float> vect4{10,20,30};
-    vector<float> vect5{20,30,40};
-    vector<float> vect6{21,31,41};
-    mat2.push_back(vect4);
-    mat2.push_back(vect5);
-    mat2.push_back(vect6);
-    vector<vector<float>> output = NaiveMatMul(mat1, mat2);
-    for (vector<float> vec : output) 
-    {
-        for (float n : vec) 
-        {
-            cout << n << " ";
-        }
-    }
+    smatrix mat1 = smatrix(vector<float> vect{10,20,30}, vector<float> vect2{20,30,40}, vector<float> vect3{21,31,41})
+    smatrix mat2 = smatrix(vector<float> vect{10,20,30}, vector<float> vect2{20,30,40}, vector<float> vect3{21,31,41})
+    smatrix output = NaiveMatMul(mat1, mat2);
+    output.view();
+    output.print();
 }
