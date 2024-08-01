6101c7a73bf2857488b981c5409dc978609d0378
Shawn <shawnschulz@berkeley.edu>
2024-08-01 10:48:19 -0700

    Start working through compiler errors for new memory model TODO: don't forget to add a std::reserve call for vector initialization

--- src/shawn_matrix.cpp
+++ src/shawn_matrix.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <vector>
 
-using namespace std::vector, std::array;
+using std::vector, std::array, std::string, std::cout, std::invalid_argument;
 
 template <typename T> class lokitrix {
 public:
@@ -14,13 +14,13 @@ public:
   //data storage for matrix operations that can be done quickly
   //single threaded. make a shared interface that uses both appropriately
   //(or just repeat yourself LOLOL)
-  typedef T* data_pointer;
-  typedef const T* const_data_pointer;
+  typedef vector<T> data_vector;
+  typedef const vector<T> const_data_vector;
   typedef size_t size_type;
   typedef T datatype;
 
-  lokitrix(T *a, int b, array<int, 2> c) {
-    create(a, b, c);
+  lokitrix(data_vector a, array<int, 2> b) {
+    create(a, b);
   }
   lokitrix(size_type n, const T& value, array<int, 2> input_dimensions){
 	  create(n, value, input_dimensions);
@@ -30,22 +30,22 @@ public:
   //be worth it for convenience in writing the matmul function
   //Having a lot of trouble accessing data to use [] operator overload, lets come back to this
   //after fixing other compiler errors
-  //T& operator[](size_type i) { 
-  //  if (dimensions[0] > 1) {
-  //    size_t output_size = dimensions[1];
-  //    array<int, 2> output_dimensions = {1, dimensions[1]};
-  //    lokitrix slice(output_size, 0, output_dimensions);
-  //    int row_start_index = this.absolute_index(i,0);
-  //    for (int i = 0; i < dimensions[1]; i++) {
-  //        //how are you gonna use the index operator for this when you haven't
-  //        //defined the index operator yet LMAO
-  //           slice[i] = this.data_start()[row_start_index + i];
-  //    } 
-  //    return lokitrix(slice, output_size, output_dimensions);
-  //  } else {
-  //    return this.data_start()[i];
-  //  }
-  //}
+  vector<T>& operator[](size_type i) { 
+    if (dimensions[0] > 1) {
+      size_t output_size = dimensions[1];
+      array<int, 2> output_dimensions = {1, dimensions[1]};
+      lokitrix slice(output_size, 0, output_dimensions);
+      int row_start_index = this.absolute_index(i,0);
+      for (int i = 0; i < dimensions[1]; i++) {
+          //how are you gonna use the index operator for this when you haven't
+          //defined the index operator yet LMAO
+             slice[i] = this.data_start()[row_start_index + i];
+      } 
+      return lokitrix(slice, output_size, output_dimensions);
+    } else {
+      return this.data_start()[i];
+    }
+  }
   lokitrix<T>& operator=(const lokitrix& right_hand_side) {
     if (&right_hand_side != this) {
       delete_lokitrix();
@@ -54,23 +54,23 @@ public:
     return *this;
   }
   void shape() {
-    cout << "row x cols: " << dimensions[0] << " " << dimensions[1] << endl;
+    cout << "row x cols: " << dimensions[0] << " " << dimensions[1] << '\n';
   }
   array<int, 2> get_dimensions() { return dimensions; }
-  data_pointer get_data() { return data; }
-  int get_size() { return data_size; }
+  data_vector get_data() { return data; }
+  int get_size() { return data.size(); }
   //const char *get_datatype() { return datatype; }
   void print(int precision = 2, int width = 5) {
     // idk we change defaults for the width that feel right
     int column_index;
-    cout << fixed << setprecision(precision);
+    cout << std::fixed << std::setprecision(precision);
     cout << "[";
     for (int row_index = 0; row_index <= dimensions[0] - 1; row_index++) {
       if (row_index != 0) {
         cout << " ";
       }
       cout << "[";
-      cout << setw(width);
+      cout << std::setw(width);
       for (int column_index = 0; column_index <= dimensions[1] - 1;
            column_index += stride[1]) {
         int abs_index = row_index * stride[0] + column_index;
@@ -82,7 +82,7 @@ public:
         if (row_index > 1000) {
           cout << "...";
           cout << "]]";
-          cout << endl;
+          cout << "\n";
           return void();
         }
       }
@@ -93,7 +93,7 @@ public:
       }
     }
     cout << "]";
-    cout << endl;
+    cout << "\n";
     return void();
   }
   void transpose() {
@@ -106,42 +106,12 @@ public:
     int absolute_index = i * stride[0] + j;
     return absolute_index;
   }
-  data_pointer data_start() { return data; };
-  const_data_pointer data_start() const { return data; };
+  const_data_vector get_const_data() const { return data; };
 
 private:
   size_type data_size;
-  //the data_pointer should be a smart pointer
-  data_pointer data;
-  //eehhh we have strayed from god, no more dynamic mem allocation 
-  //lets make 6 create functions that make a static array of size
-  //1mb, 100 mb, 1gb, 8gb, 32gb and 64gb
-  //ideally we use make_unique or something somehow to add some
-  //attempt at memory safety
-  //
-  //create will default to an 8gb lokitrix, since that seems like
-  //a reasonable amount for a neural network sized dataset
-  //
-  //data will be a smart pointer to an appropriately sized std::array
-  void create(data_pointer, size_type, array<int, 2>);
-  void create_4mb(data_pointer, size_type, array<int, 2>);
-  void create_100mb(data_pointer, size_type, array<int, 2>);
-  void create_1gb(data_pointer, size_type, array<int, 2>);
-  void create_8gb(data_pointer, size_type, array<int, 2>);
-  void create_32gb(data_pointer, size_type, array<int, 2>);
-  void create_64gb(data_pointer, size_type, array<int, 2>);
-
-  //data will be a smart pointer to std::vec
-  void create_dyn(data_pointer, size_type, array<int, 2>);
-  //all of these can be overloaded to make empty versions with repititous input
-  //data
-  //are we repeating ourselves? yes. should we really have a shared interface?
-  //also yes. but i think this is the only way to do this without dynamic
-  //mem allocation, and while it was a fun side thing to reimpliment std::vec
-  //the point of this library is HPC and this will make that a lot easier
-  //i think we don't need to store what type we made? can check it based on
-  //the size of array since these are now gonna be std::arrays
-  //
+  data_vector data;
+  void create(data_vector, array<int, 2>);
   //old dynamic mem allocation functions
   //void create();
   //void create(size_type, const T&, array<int, 2>);
@@ -171,21 +141,13 @@ private:
 //  }
 //}
 template <class T>
-//initializes data from an input array with no shape, default allocates 
-//roughly 1 gb worth of f32 array (e.g. if you choose to use double instead
-//you'll actually use 2 gb of memory, if u use a f16 you use 500 mb of memory
-//etc.
-void lokitrix<T>::create(data_pointer input, size_type n, array<int, 2> input_dimensions) {
-  array<T, 250000000> data;
+void lokitrix<T>::create(data_vector input, array<int, 2> input_dimensions) {
   //this seems dangerous, could easily dereference a null ptr
-  if (!(data.size() > n)) {
-          throw invalid_argument("Given data exceeds size of allocated array");
-          }
-  std::copy(*input, data);
+  data = input;
   dimensions = input_dimensions;
   stride[0] = (data_size / input_dimensions[0]);
   stride[1] = 1;
-  if (data_size % dimensions[1] != 0 ||
+  if (input.size() % dimensions[1] != 0 ||
       dimensions[0] != data_size / dimensions[1]) {
     throw invalid_argument("Given data size and dimensions don't match");
   }
@@ -195,9 +157,9 @@ template <class T>
 void lokitrix<T>::delete_lokitrix()
 {
   if (data) {
-    delete_lokitrix(data);
-    delete_lokitrix(dimensions);
-    delete_lokitrix(stride);
+    delete(data);
+    delete(dimensions);
+    delete(stride);
   }
 }
 // template <typename T>
@@ -270,25 +232,22 @@ lokitrix<T> CPUMatMul(lokitrix<T> mat1, lokitrix<T> mat2) {
         int mat1_abs_index = mat1.absolute_index(shared_index, j);
         int mat2_abs_index = mat2.absolute_index(i, shared_index);
 	int mat3_abs_index = output_data.absolute_index(mat1_abs_index, mat2_abs_index);
-        new_cell_value = mat1.data_start()[mat1_abs_index] * mat2.data_start()[mat2_abs_index];
-        output_data.data_start()[mat3_abs_index] = new_cell_value;
+        new_cell_value = mat1.get_data()[mat1_abs_index] * mat2.get_data()[mat2_abs_index];
+        output_data.get_data()[mat3_abs_index] = new_cell_value;
       }
     }
   }
-  // it doesn't feel intuitive that this will work, will the actual data
-  // output_data points to still be there once we leave this scope? be careful
-  // about this
   return output_data;
 }
 
 int main() {
-  array<float, 12> data1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-  array<float, 12> data2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  vector<float> data1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  vector<float> data2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
   array<int, 2> dims = {4, 3};
-  lokitrix mat1(&input, 12, dims);
-  lokitrix mat2(&input2, 12, dims);
+  lokitrix mat1(data1, dims);
+  lokitrix mat2(data2, dims);
   mat1.print();
-  cout << mat1.absolute_index(1, 2) << endl;
+  cout << mat1.absolute_index(1, 2) << "\n";
   mat1.transpose();
   mat1.print();
   lokitrix mat3 = CPUMatMul(mat1, mat2);
