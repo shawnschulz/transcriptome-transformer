27c41c60d8e6c2adf2d545e690c0b7ab7284d01f
Shawn Schulz <shawnschulz@berkeley.edu>
2024-08-18 22:00:51 -0700

    Add working completion adder with documentation

--- rust_loki/src/auto_complete.rs
+++ rust_loki/src/auto_complete.rs
@@ -1,46 +1,69 @@
-#![crate_name = "auto_complete"]
-use std::process::Command;
-///the purpose of this tool is to generate recommendations and then update
-///autocomplete.sh
+///auto_complete contains the methods and structures for making completions and serializing.
+
+
+///Checks where if a string representing a run command exists in the commands list, then inserts at the front if it does. If it doesn't it inserts it
+/// in the back category
+/// 
+/// ## Examples 
+/// 
+/// ```
+///let mut test_commands: [Vec<String>; 2]= [vec![], vec!["git add -a -m 'Add README.md'".to_string()]];
+///assert!(insert_command("git add -a -m '".to_string(), &mut test_commands).is_ok());
+///assert_eq!([vec![], vec!["git add -a -m '".to_string(), "git add -a -m 'Add README.md'".to_string()]], test_commands);
 ///
-///The actual running of loki should be handled by a different tool, which
-///will either run commands which can be tab autocompleted or 
-///take in a prompt and generate a command 
-
-fn add_auto_complete_to_sh(input_string: &str, fp: &str){
-    ///Given a properly formatted input_string, appends lines to auto_complete.sh
-    ///here's an example of a complete autocomplete string:
-    ///complete -W "'cd /home/shawn/Documents' 'python3 -m pbd sort.py'" lo
-    return
+///let mut test_commands: [Vec<String>; 2]= [vec![], vec!["git add -a -m 'Add README.md'".to_string()]];
+///assert!(insert_command("git add -a -m 'Add README.md'".to_string(), &mut test_commands).is_ok());
+///assert_eq!([vec!["git add -a -m 'Add README.md'".to_string()], vec![]], test_commands);
+/// ```
+/// 
+/// ## Errors
+/// 
+/// insert_command will error if receives empty string, only white space or only new lines as a command
+pub fn insert_command<'a>(new_command: String, commands: &'a mut [Vec<String>; 2]) -> Result<&'static str, &'static str>
+{
+    //Check if the entered command is just whitespace or newlines
+    if new_command.len() == 0 || new_command.chars().all(|x| x == ' ') || new_command.chars().all(|x: char| x == '\n') || new_command == ""
+    { 
+        return Err("Error: received either an empty string or only whitespace as a command")
+    }
+    for category_i in 0..commands.len()
+    {
+        for i in 0..commands[category_i].len() 
+        {
+            if new_command == commands[category_i][i]
+            {
+                let repeat_command = commands[category_i].remove(i);
+                commands[0].insert(0, repeat_command.to_string());
+                return Ok("Command found in previous commands, placing at front of commands");
+            }
+        }
+    }
+    //Actually have to reborrow our commands here
+    commands[1].insert(0, new_command);
+    return Ok("Command not run before, placing at front of commands used once before")
 }
 
-//fn get_initial_commands() -> Vec<u8>{
-//    ///Uses comgen -c in an abi call to get all installed commands to generate
-//    ///recommendations. Only should be used at beginning, not for fine
-//    ///tuning recommendations
-//    let commands = Command::new("cmd")
-//        .arg("compgen")
-//        .arg("-c")
-//        .output()
-//        .expect("failed to execute compgen");
-//    return commands.stdout
-//}
+// pub fn write_to_file(commands:Vec<String>) -> Result<&'static str, &'static str>
+// {
 
-pub fn get_user_gen_commands<'a>(commands_fp: &'a str, ret_type: &'a str) -> &'a str{
-    ///Takes a one column string .txt or .tsv containing previously run commands
-    ///dataset, returns a newline seperated string or csv 
-    return ""
-} 
+// }
 
-pub fn generate_recommendation_strings(model_path:&str) -> &str {
-    ///Generates reccomendation strings using llama_cpp, formats them properly
-    ///and appends all to auto_complete.sh. if possible, sources it too
-    ///For formatting, all strings need to use "lo" for the autocomplete
-    return ""
-}
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
 
-fn main(){
-    let a:&str = "Hello World";
-    println!("{}", a);
-    println!("{:?}", get_initial_commands());
+    #[test]
+    fn test_insert_new_command() {
+        let mut test_commands: [Vec<String>; 2]= [vec![], vec!["git add -a -m 'Add README.md'".to_string()]];
+        assert!(insert_command("git add -a -m '".to_string(), &mut test_commands).is_ok());
+        assert_eq!([vec![], vec!["git add -a -m '".to_string(), "git add -a -m 'Add README.md'".to_string()]], test_commands);
+    }
+    #[test]
+    fn test_insert_old_command() {
+        let mut test_commands: [Vec<String>; 2]= [vec![], vec!["git add -a -m 'Add README.md'".to_string()]];
+        assert!(insert_command("git add -a -m 'Add README.md'".to_string(), &mut test_commands).is_ok());
+        assert_eq!([vec!["git add -a -m 'Add README.md'".to_string()], vec![]], test_commands);
+    }
+
 }
--- rust_loki/src/bin/_loki_complete.rs
+++ rust_loki/src/bin/_loki_complete.rs
@@ -1,10 +1,66 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+// //TODO: generate subcommands from a file, write commands you run through lo in shell to that file
+// //two levels of subdivision, the subcommands and the commands
+
+// ///Outlines all of the types of commands possible that are flagged in compgen
+// enum CommandType {
+//     arg,
+//     directory,
+//     host_name,
+// }
+
+// ///A command, which contains a command name and its subcommands/args
+// struct LoCommand<'a> {
+//     command_name: String,
+//     sub_commands: Vec<&str>,
+//     arg_type: CommandType,
+// }
+
+// ///A list of commands, which contains methods to produce lists of commands
+// struct CommandsList {
+//     commands: Vec<LoCommand>,
+// }
+
+// ///Make a CommandsList Struct from reading a file
+// fn commands_from_file(file_path: &str) -> Vec<String> {
+//     let file = File::open(file_path)?;
+//     let mut s = String::new();
+//     file.read_to_string(&mut s)?;
+//     let result: Vec<String> = file.read_to_string(&mut s).lines().collect();
+//     return result;
+
+// }
+
+// ///Write LoCommands to a file
+// fn write_lo_to_file(commands: LoCommand) {
+// }
+
+// ///Our bash executable calls
+
+// ///Generate the commands list using a bash executable
+// fn generate_commands_list(file_path: String) {
+// }
+
+// ///Run complete -C on the compiled executable, this is gonna need to be moved elsewhere but can sit here for now
+// fn run_complete() {
+
+// }
+
 fn main() {
-    let subcommands = vec!["add", "commit"];                                                                       
-        let args : Vec<String> = std::env::args().collect();                                                           
-        let word_being_completed = &args[2];                                                                           
-        for subcommand in subcommands { 
-           if subcommand.starts_with(word_being_completed) {
-               println!("{}", subcommand);
-           }
-        }  
+    // let commands: Vec<String> = commands_from_file("./commands.txt");
+    // //This lets us get the sub command being completed, but we need a 
+    // //further subdivision by command as a whole
+    // let args : Vec<String> = std::env::args().collect();                                                           
+    // let word_being_completed = &args[2];                                                                           
+
+    // //The order of the commands in commands.txt matters, should create function that reorders from buffer based on overlapping strings
+    // for command in commands {
+    //     if command.starts_with(word_being_completed) {
+    //         println!("{}", command);
+    //     }
+    // }
 }
